ebfa56dd3bae2462d16cea543d0b7ad7ba335686
stofstik <verstegen.daan@gmail.com>
2016-10-15 20:55:31 +0200

    added relations and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Seeding seeding seeding, where are all the plants?
+
+Song.delete_all
+Artist.delete_all
+
+sleaford_mods = Artist.create( { name: "Sleaford Mods" } )
+rev_peyt = Artist.create( { name: "The Reverend Peyton's Big Damn Band" } )
+allah_las = Artist.create( { name: "Allah-Las" } )
+
+Song.create( { name: "Tied Up In Nottz", artist: sleaford_mods} )
+Song.create( { name: "Air Conditioning", artist: sleaford_mods} )
+
+Song.create( { name: "Sure Feels Like Rain", artist: rev_peyt} )
+Song.create( { name: "Everything's Raising", artist: rev_peyt} )
+
+Song.create( { name: "Catamaran", artist: allah_las} )
+Song.create( { name: "Don't You Forget It", artist: allah_las} )
