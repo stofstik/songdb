3f256f1c527e1b34045be24ef36c0fafdd112bfc
stofstik <verstegen.daan@gmail.com>
2016-10-22 12:38:39 +0200

    added sorting links and image placeholder

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,11 +1,24 @@
 class ArtistsController < ApplicationController
+
   def index
-    @artists = Artist.all
+    if params[:order_by] == 'created_at'
+      @artists = Artist.all.order(:created_at)
+    elsif params[:order_by] == 'name'
+      @artists = Artist.all.order(:name)
+    else
+      @artists = Artist.all
+    end
   end
 
   def show
     @artist = Artist.find(params[:id])
     @songs = @artist.songs
+  end
+
+  private
+
+  def order_params
+    require(:order_by).permit(:created_at, :name)
   end
 
 end
--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,12 @@
 class Artist < ApplicationRecord
   has_many :songs, dependent: :destroy
+
+  def self.ordered_by_name
+    Artist.all.order(:name)
+  end
+
+  def self.ordered_by_created
+    Artist.all.order(:created_at)
+  end
+
 end
