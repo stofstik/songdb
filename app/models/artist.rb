65c27b79140243c960f431a1f5bad3c3a91c27fd
stofstik <verstegen.daan@gmail.com>
2016-10-23 22:04:04 +0200

    Use placeholder when no photo uploaded

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -5,7 +5,7 @@ class Artist < ApplicationRecord
   validates :name, presence: true, length: { maximum: 128 }
 
   def photo_url
-    photo.image
+    photo ? photo.image : "http://placehold.it/350x350"
   end
 
   def self.ordered_by_name
